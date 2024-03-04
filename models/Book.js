6e614babfac4bc3aea32dff0ffea065c103a9f1f
Falk König <fk@falkkoenig.de>
2024-03-04 15:14:16 +0100

    add route authors #2

--- models/Book.js
+++ models/Book.js
@@ -6,10 +6,9 @@ const userSchema = new Schema(
   {
     title: { type: String, required: true },
     subtitle: { type: String, required: false },
-    author: { type: String, required: true },
     year: { type: Number, required: false },
     isbn: { type: String, required: true },
-    authorId: { type: Schema.Types.ObjectId, ref: "Author" },
+    author: { type: Schema.Types.ObjectId, ref: "Author" },
   },
   { versionKey: false }
 );
