2cceb28583f6b11ae2ec46ccf1e00dee6feef04c
Falk König <fk@falkkoenig.de>
2024-03-04 16:30:04 +0100

    add authors books route

--- app.js
+++ app.js
@@ -39,11 +39,29 @@ app.get("/books", async (req, res) => {
 
 app.get("/authors", async (req, res) => {
   await connect();
-  const authors = await Author.find().populate("book", "books");
+  const authors = await Author.find();
   if (!authors.length) {
     return res.json({ message: "Cannot find any authors!" });
   }
   return res.json(authors);
+});
+
+app.get("/:author", async (req, res) => {
+  await connect();
+  const { author } = req.params;
+
+  const { _id: authorId } = (await Author.findOne({ name: author })) || { _id: null };
+
+  console.log(author);
+  console.log(authorId);
+
+  if (!authorId) {
+    return res.status(404).json({ message: "Cannot find this author!" });
+  }
+
+  const books = await Book.find({ author: author });
+
+  return res.json(books);
 });
 
 const server = app.listen(port, () => console.log(`Express app listening on port ${port}!`));
