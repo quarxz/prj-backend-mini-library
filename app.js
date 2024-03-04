288f229dab563bcb1192c81dc847442690176982
Falk König <fk@falkkoenig.de>
2024-03-04 14:32:38 +0100

    add cors #2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
